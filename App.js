1a9db6d5dc62b9f2d66145504c39e9c9ea31b0b0
brknce <brknce19@gmail.com>
2020-11-12 17:12:14 +0300

    input components

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { Switch, Text, TextInput, View } from "react-native";
 import Card from "./app/components/Card";
 import ListingDetailScreen from "./app/screens/ListingDetailScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
@@ -8,7 +8,28 @@ import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingScreen from "./app/screens/ListingScreen";
+import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Camera", value: 3 },
+];
 
 export default function App() {
-  return <ListingScreen></ListingScreen>;
+  const [category, setCategory] = useState(categories[0]);
+
+  return (
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
+  );
 }
